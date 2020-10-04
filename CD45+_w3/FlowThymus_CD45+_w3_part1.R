afe6c41f5376d557ad8f50629975eeec48967403
france-hub <francemazziotta@gmail.com>
2020-10-04 18:14:18 -0400

    definitive scripts CD45+ w3

--- CD45+_w3/FlowThymus_CD45+_w3_part1.R
+++ CD45+_w3/FlowThymus_CD45+_w3_part1.R
@@ -39,17 +39,17 @@ getwd()
 PrimaryDirectory <- getwd()
 PrimaryDirectory
 
-# Define csv directory
+# Define directory that contains csv files
 csv <- "csv"
 csvDirectory <- paste(PrimaryDirectory, csv, sep = "/")
 dir.create(csvDirectory)
 
-# Define workingDirectory
+# Define working directory
 wdName <- "WorkingDirectory"
 workingDirectory <- paste(PrimaryDirectory, wdName, sep = "/")
 dir.create(workingDirectory)
 
-#Define csv2fcsDirectory
+#Define csv to fcs directory
 csv2fcsDirectory <- "csv2fcsDirectory"
 csv2fcsDirectory <- paste(PrimaryDirectory, csv2fcsDirectory, sep = "/")
 dir.create(csv2fcsDirectory)
@@ -72,10 +72,11 @@ for(i in c(1:length(CSVfiles))){
 FCSfiles <- list.files(csv2fcsDirectory, pattern = ".fcs$", full = FALSE)
 fs <- read.flowSet(files = FCSfiles, path = csv2fcsDirectory, truncate_max_range = FALSE)
 
-#Look at the colnames(fs) and rename those of interest with the specific markers
-colnames(fs)
+#Look at the colnames(fs) and rename those of interest with the specific markers of interest
+colnames(fs) 
 colnames(fs)[7:15] <- c("c-kit", "ki-67", "CD44", "CD45", "CD4", "CD8", "Thy1", "CD3", 
                         "CD25")
 
-save(list = ls(), file =("thymus_step1.rds"))
+#Save
+save(list = ls(), file =("FlowThymus_CD45+_w3_part1.rds"))
 
--- CD45+_w3/FlowThymus_sce_CD45+_w3_part2.R
+++ CD45+_w3/FlowThymus_sce_CD45+_w3_part2.R
@@ -1,98 +1,16 @@
 rm(list = ls())
 
 ###############################
-## INSTALL REQUIRED PACKAGES ##
-###############################
-
-if(!require('rstudioapi')) {
-  install.packages('rstudioapi')
-}
-if(!require('devtools')){
-  install.packages("devtools")
-}
-if(!require('flowCore')){
-  install.packages("flowCore")
-}
-if(!require('cytofCore')){
-  devtools::install_github("nolanlab/cytofCore")
-}
-if(!require('cytofkit2')){
-  #do not update hexbin
-  devtools::install_github("JinmiaoChenLab/cytofkit2")
-}
-if(!require('FlowSOM')){
-  install.packages("FlowSOM")
-}
-if(!require('cluster')){
-  install.packages("cluster")
-}
-if(!require('Rtsne')){
-  install.packages("Rtsne")
-}
-if(!require('ggplot2')){
-  install.packages("ggplot2")
-}
-if(!require('dplyr')){
-  install.packages("dplyr")
-}
-if(!require('ggthemes')){
-  install.packages("ggthemes")
-}
-if(!require('RColorBrewer')){
-  install.packages('RColorBrewer')
-}
-if(!require("uwot")){
-  install.packages("uwot")
-}
-if(!require("CATALYST")){
-  BiocManager::install("CATALYST")
-}
-if(!require("diffcyt")){
-  BiocManager::install("diffcyt")
-}
-if(!require("stringr")){
-  BiocManager::install("stringr")
-}
-if(!require("scran")){
-  BiocManager::install("scran")
-}
-if(!require("scater")){
-  BiocManager::install("scater")
-}
-if(!require("ggcyto")){
-  BiocManager::install("ggcyto")
-}
-if(!require("SingleCellExperiment")){
-  BiocManager::install("SingleCellExperiment")
-}
-if(!require("flowWorkspace")){
-  BiocManager::install("flowWorkspace")
-}
-if(!require("reshape2")){
-  BiocManager::install("reshape2")
-}
-if(!require("ggrepel")){
-  BiocManager::install("ggrepel")
-}
-if(!require("knn.covertree")){
-  devtools::install_github('flying-sheep/knn.covertree')
-}
-if(!require("theislab/destiny")){
-  devtools::install_github('theislab/destiny')
-}
-
-###############################
 ### LOAD REQUIRED PACKAGES ####
 ###############################
 
 suppressPackageStartupMessages({
   library(rstudioapi)
+  library(devtools)
   library(flowCore)
   library(cytofCore)
-  library(cytofkit2)
   library(FlowSOM)
   library(cluster)
-  library(Rtsne)
   library(ggplot2)
   library(dplyr)
   library(ggthemes)
@@ -126,7 +44,7 @@ getwd()
 PrimaryDirectory <- getwd()
 PrimaryDirectory
 
-#load("thymus_step1.rds")
+load("FlowThymus_CD45+_w3_part1.rds")
 
 ############################
 ######BUILD SCE OBJECT######
@@ -143,40 +61,44 @@ keyword(fs[[1]])[[keep$cyt]] <- "FACS"
 ##Building panel dataframe
 # Define channels of interest and marker_class
 fcs_colname <- colnames(fs)[7:15]
-
+antigen <- fcs_colname  
 marker_class <- c("type", "state", rep("type", 7))
-antigen <- fcs_colname
-length(marker_class) == length(fcs_colname)
+length(marker_class) == length(fcs_colname) #check 
 
-#Panel
+# Create panel
 panel <- cbind(fcs_colname, antigen, marker_class)
 panel <- as.data.frame(panel)
-all(panel$fcs_colname %in% colnames(fs))
+all(panel$fcs_colname %in% colnames(fs)) #check
 
 ##Building metadata dataframe
+#Define condition
 condition <- FCSfiles
 condition <- word(condition, 1, sep = "_")
 condition[grepl("C", condition)] <- "Ctrl"
 condition[grepl("Z", condition)] <- "Zd"
 
-
+#Define patient_id
 patient_id <- FCSfiles
 patient_id <- word(patient_id, 2, sep = "_")
 
+#Define sample_id
 sample_id <- paste(patient_id, condition, sep = "_")
 
+#Define file_name
 file_name <- FCSfiles
 
-md <- cbind(file_name, sample_id, condition, patient_id)
-md <- data.frame(md)
+#Create metadata dataframe
+md <- as.data.frame(cbind(file_name, sample_id, condition, patient_id))
+
 
 #Check if ids and md$file_name are the same
 ids <- c(fsApply(fs, identifier))
 ids%in%md$file_name
 
-##SCE object
+# create SingleCellExperiment object
 sce <- CATALYST::prepData(fs, panel = panel, md = md, transform = FALSE, features = panel$fcs_colname)
 sce@assays@data$exprs <- sce@assays@data$counts
+assays(sce)
 
 ## QC
 # Density plots
@@ -184,11 +106,11 @@ p <- plotExprs(sce, color_by = "condition")
 p$facet$params$ncol <- 4
 p
 
-# Counts
+# Check number of cells for each sample
 n_cells(sce)
 plotCounts(sce, group_by = "sample_id", color_by = "condition")
 
-# Multidimensional scaling (MDS)
+# Pseudo-bulk multidimensional scaling (pbMDS)
 CATALYST::pbMDS(sce, color_by = "condition", label = "patient_id")
 
 # Non Redundancy Score (NRS)
@@ -197,28 +119,28 @@ plotNRS(sce, features = type_markers(sce), color_by = "condition")
 #Delete outlier 011
 sce <- filterSCE(sce, patient_id != "011")
 
-## Clustering
-#FlowSOM
+# Run FlowSOM and ConsensusClusterPlus
 set.seed(1234)
 sce <- cluster(sce,
                xdim = 10, ydim = 10, maxK = 20, seed = 1234)
 delta_area(sce)
+
+#Check Heatmap
 plotExprHeatmap(sce, features = "type",
                 by = "cluster_id", k = "meta16", 
-                bars = TRUE, perc = TRUE, col_anno = TRUE, scale = "last")
+                bars = TRUE, perc = TRUE, col_anno = TRUE, scale = "last", bin_anno = TRUE)
 
-#Filter very small clusters (< 0.4%)
+#Remove C16 - 0.17%
 sce <- filterSCE(sce, cluster_id %in% c(1:15), k = "meta16")
 
-#Run UMAP algorithm with a downsampling of 3000
-set.seed(1234)
+# Run dimensionality reduction - UMAP
 n_cells <- 3000
-exaggeration_factor <- 12.0
-eta <- n_cells/exaggeration_factor
+n_events <- min(n_cells(sce))
+if(!(n_cells < n_events))
+  n_cells <- n_events
 sce <- runDR(sce, dr =  "UMAP", cells = n_cells, features = "type")
 
-#Plot UMAP
-plotDR(sce, "UMAP", color_by = "condition")
+# Plot UMAP
 plotDR(sce, "UMAP", color_by = type_markers(sce)) #markers distribution
 plotDR(sce, "UMAP", color_by = "meta16") + facet_wrap("condition") + 
   guides(color = guide_legend(ncol = 2, override.aes = list(size = 3))) #different conditions
@@ -235,34 +157,33 @@ plotAbundances(sce, k = "meta16", by = "cluster_id", group_by = "condition")
 
 ##Add annotations
 #Read annotation excel file
-annotation_table <- readxl::read_excel(file.choose())
+annotation_table <- readxl::read_excel("annotation.xlsx")
 annotation_table
 
-#convert to factor with merged clusters in desired order
+#Convert to factor with merged clusters in the desired order
 annotation_table$new_cluster <- factor(annotation_table$new_cluster, 
                                        levels = c("DN2a", "DN1", "ETP", "DN2b", "DN3",  "Non lymphoid cells", "DP CD90 low",
                                                   "CD4+", "CD8+", "CD4+ immature", "ISP", "DN4", "DP CD90 intermediate", 
                                                   "DP CD90 high"))
-# apply manual annotation
+#Apply manual annotation
 sce <- mergeClusters(sce, k = "meta16", 
                      table = annotation_table, id = "cluster_annotation", overwrite = TRUE)
 
 #Heatmap with annotations
 plotExprHeatmap(sce, features = "type", 
-                by = "cluster_id", k = "meta16", m = "cluster_annotation", scale = "last")
+                by = "cluster_id", k = "meta16", m = "cluster_annotation", scale = "last", bin_anno = TRUE)
 
 plotExprHeatmap(sce, features = "type",
-                by = "cluster_id", k = "cluster_annotation", scale = "last")
+                by = "cluster_id", k = "cluster_annotation", scale = "last", bin_anno = TRUE)
 
 #UMAP with annotations
 dev.off()
-levels(cluster_id) == levels("cluster")
 plotDR(sce, "UMAP", color_by = "cluster_annotation") + facet_wrap("condition")
 plot <- plotDR(sce, "UMAP", color_by = "cluster_annotation") + facet_wrap("condition")
 plot + geom_density2d(binwidth = 0.006, colour = "black")
 
-#Abundancies with the annotations
+#Plot abundancies 
 plotAbundances(sce, k = "cluster_annotation", by = "cluster_id", shape_by = NULL)
 
 # Save workspace 
-save(list = ls(), file = "CD45+_timo_2nd.rds")
+save(list = ls(), file = "FlowThymus_sce_CD45+_w3_part2.rds")
